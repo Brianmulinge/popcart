dc158102610cf90e643f75c7d5190e4d13aa18a4
Brianjaden <brianjaden45@gmail.com>
2023-02-12 06:11:54 -0600

    remove comma

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -8,7 +8,9 @@ function ProductItem() {
     <div className=" rounded-lg m-10 h-80 w-80 border">
       <Image className="rounded-t-lg" alt="productimage" src={pic} />
       <div className="p-4">
-        <h1 className="font-semibold text-lg">Men's Fashion Popcart Tshirt</h1>
+        <h1 className="font-semibold text-lg">
+          Men`&apos;`s Fashion Popcart Tshirt
+        </h1>
         <h1 className="text-sm font-semibold">The perfect tshirt design</h1>
         <StarIcon className="h-4 w-4" />
         <button className="font-semibold rounded-full px-6 py-3 border mt-2">
